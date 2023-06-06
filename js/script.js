6c231981ba1ff1b3035670d25f0561f26a241221
yb-youness <ben.youness.bahi@gmail.com>
2023-06-06 15:49:47 +0100

    Adding read me file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const global = { curentPage: window.location.pathname,
-  serach : {
+  search : {
     term : '',
     type : '',
     page : 1,
-    totalPages:1
+    totalPages:1,
+    totalResults:0
   },
   api :{
     apiKey:"5a2ffd81b33bc004f6cc414f79d7f774",
@@ -300,13 +301,17 @@ async function search(){
    // get the data from the Url 
   const queryString = window.location.search;
   const urlPrams    = new URLSearchParams(queryString);
-  global.serach.type = urlPrams.get('type');
-  global.serach.term = urlPrams.get('search-term');
+  global.search.type = urlPrams.get('type');
+  global.search.term = urlPrams.get('search-term');
 
-  if(global.serach.term !== '' && global.serach.term !==null){
+  if(global.search.term !== '' && global.search.term !==null){
       // make request and display results
-      const {results,total_pages,page} = await serachApiData();
+      const {results,total_pages,page,total_results} = await serachApiData();
         
+        global.search.page =page;
+        global.search.totalResults =total_results;
+        global.search.totalPages =total_pages;
+
       if(results.length==0){
         showAlert("Element Not Found  ","alert-error");
         return;
@@ -318,45 +323,88 @@ async function search(){
   }
 }
 function displaySearchResults(results){
+  // Clear prev results 
+  document.querySelector("#search-results").innerHTML =""; 
+  document.querySelector("#search-results-heading").innerHTML ="";
+  document.querySelector("#pagination").innerHTML ="";  
   results.forEach((result) => {
     const div = document.createElement("div");
     div.classList.add("card");
     div.innerHTML = `<div>
-   <a href="${global.serach.type}-details.html?id=${result.id}">
+   <a href="${global.search.type}-details.html?id=${result.id}">
      ${
        result.poster_path
          ? `<img 
      src="https://image.tmdb.org/t/p/w500${result.poster_path}"
      class="card-img-top"
-     alt="${global.serach.type === 'movie' ? result.title : result.name}"
+     alt="${global.search.type === 'movie' ? result.title : result.name}"
    />`
          : `<img
    src="../images/no-image.jpg"
    class="card-img-top"
-   alt="${global.serach.type === 'movie' ? result.title : result.name}"
+   alt="${global.search.type === 'movie' ? result.title : result.name}"
   />`
      } 
    </a>
    <div class="card-body">
-     <h5 class="card-title">${global.serach.type === 'movie' ? result.title : result.name}</h5>
+     <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
      <p class="card-text">
-       <small class="text-muted">Release: ${global.serach.type === 'movie' ? result.release_date : result.first_air_date}</small>
+       <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
      </p>
    </div>
    </div>`;
+    
+    document.querySelector("#search-results-heading").innerHTML =`
+       <h2>${results.length} of ${global.search.totalResults}  Results for ${global.search.term} </h2>
+    `;
     document.querySelector("#search-results").appendChild(div);
+   
   });
+   //Show pagination 
+   displayPagination();
+}
+
+function displayPagination(){
+  const div = document.createElement("div");
+  div.classList.add("pagination");
+  div.innerHTML =`<button class="btn btn-primary" id="prev">Prev</button>
+                  <button class="btn btn-primary" id="next">Next</button>
+                   <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>`;
+
+                   document.querySelector("#pagination").appendChild(div);
+        // Disable prev Button if on the first page 
+        if(global.search.page == 1){
+          document.querySelector("#prev").disabled = true;
+        }    
+        // Disable Next Button If thelast Page 
+        if(global.search.page == global.search.totalPages){
+          document.querySelector("#next").disabled = true;
+        }  
+        // Next Page
+        document.querySelector("#next").addEventListener("click",async()=>{
+         global.search.page++;
+         const {results , total_pages} = await serachApiData();
+         displaySearchResults(results);
+        });  
+        
+          // Prev Page
+          document.querySelector("#prev").addEventListener("click",async()=>{
+            global.search.page--;
+            const {results , total_pages} = await serachApiData();
+            displaySearchResults(results);
+           });      
+  
 }
 
 async function serachApiData(){
   const API_KEY =  global.api.apiKey;
   const API_URL = global.api.apiUrl;
-  const searchTerm = global.serach.term;
+  const searchTerm = global.search.term;
 
   showSpinner();
 
   const resp = await fetch(
-    `${API_URL}search/${global.serach.type}?api_key=${API_KEY}&language=en-US&query=${searchTerm}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
   const data = await resp.json();
   hideSpinner();
